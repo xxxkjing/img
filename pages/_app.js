0bfc6afb0eb8971811b7631c3c5fc31dbcd6307b
MinusTen <xkjing@outlook.com>
2025-03-02 18:42:46 +0800

    Add files via upload

---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+// pages/_app.js
+import '../public/global.css';
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
-// pages/index/index.js
+// pages/index.js (直接映射到根路径)
+import Head from 'next/head';
 import '../public/global.css';
-import './style.css';
 
 export default function Home() {
   const handleUpload = async (file) => {
     const formData = new FormData();
     formData.append('file', file);
     const res = await fetch('/api/proxy', { method: 'POST', body: formData });
-    return res.json();
+    return await res.json();
   };
 
   return (
